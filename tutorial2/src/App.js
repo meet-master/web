ca9a383cda87608e17c457c116d1233f252d335b
Meet Master <meet.master@dal.ca>
2023-01-31 22:32:28 -0400

    completed tutorial 2

---
+++ tutorial2/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi,  Meet Master (B00903590). React application is working!!!!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
